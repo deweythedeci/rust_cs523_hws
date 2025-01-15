a5923928754bf265c888013ed6d272c022f03595
Benjamin Chong <ben.c.chong@gmail.com>
2025-01-14 17:19:40 -0800

    init hw1

---
+++ hw1/target/debug/deps/hw1-b72b9c3b217c3017.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/dewey/Documents/code/rust_cs523/rust_cs523_hws/hw1/target/debug/deps/hw1-b72b9c3b217c3017: src/main.rs
+
+/mnt/c/Users/dewey/Documents/code/rust_cs523/rust_cs523_hws/hw1/target/debug/deps/hw1-b72b9c3b217c3017.d: src/main.rs
+
+src/main.rs:
---
+++ hw1/target/debug/hw1.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/dewey/Documents/code/rust_cs523/rust_cs523_hws/hw1/target/debug/hw1: /mnt/c/Users/dewey/Documents/code/rust_cs523/rust_cs523_hws/hw1/src/main.rs
