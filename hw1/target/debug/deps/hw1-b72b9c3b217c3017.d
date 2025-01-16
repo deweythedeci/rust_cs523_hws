36d6f9b7feb6a871192c18c5f45ca5954e306511
Benjamin Chong <ben.c.chong@gmail.com>
2025-01-16 12:14:37 -0800

    added .gitignore

--- hw1/target/debug/deps/hw1-b72b9c3b217c3017.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/dewey/Documents/code/rust_cs523/rust_cs523_hws/hw1/target/debug/deps/hw1-b72b9c3b217c3017: src/main.rs
-
-/mnt/c/Users/dewey/Documents/code/rust_cs523/rust_cs523_hws/hw1/target/debug/deps/hw1-b72b9c3b217c3017.d: src/main.rs
-
-src/main.rs:
